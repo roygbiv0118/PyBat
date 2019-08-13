58076f8e83498774e9f998f8f2425c2e655b3b01
zhuyun <zhuy@chbiomedical.com>
2019-08-13 15:35:13 +0800

    update search

--- serarch_dlt.py
+++ serarch_dlt.py
@@ -9,8 +9,10 @@ if __name__ == '__main__':
     c = conn.cursor()
     cursor = c.execute("SELECT * FROM dlt")
     conn.commit()
-    myr = [1, 18, 19, 27, 29]
-    myb = [2, 4, 8]
+    # myr = [1, 18, 19, 27, 29]
+    # myb = [2, 4, 8]
+    myr = [22, 24, 29, 31, 35]
+    myb = [4, 11]
 
     # myr = [2, 15, 17, 25, 31]
     # myb = [3, 6]
@@ -18,8 +20,10 @@ if __name__ == '__main__':
     # myb = [3, 8]
     # myr = [5, 10, 14, 17, 31]
     # myb = [4, 5]
+    all_balls = []
 
     for row in cursor:
+        all_balls.append(row)
         rb = []
         rb.append(row[1])
         rb.append(row[2])
@@ -37,10 +41,42 @@ if __name__ == '__main__':
         for b in myb:
             if b in bb:
                 cBlue+=1
-        if cRed>=3:
+        if cRed>3 or (cRed == 3 and cBlue == 2):
             print(row[0], cRed, cBlue)
                 
     conn.close()
 
+
+    # print(len(all_balls))
+    # for rw in all_balls:
+    #     mrb = []
+    #     mrb.append(rw[1])
+    #     mrb.append(rw[2])
+    #     mrb.append(rw[3])
+    #     mrb.append(rw[4])
+    #     mrb.append(rw[5])
+    #     mbb = []
+    #     mbb.append(rw[6])
+    #     mbb.append(rw[7])
+    #     for row in all_balls:
+    #         rb = []
+    #         rb.append(row[1])
+    #         rb.append(row[2])
+    #         rb.append(row[3])
+    #         rb.append(row[4])
+    #         rb.append(row[5])
+    #         bb = []
+    #         bb.append(row[6])
+    #         bb.append(row[7])
+    #         cRed = 0
+    #         cBlue = 0
+    #         for r in mrb:
+    #             if r in rb:
+    #                 cRed+=1
+    #         for b in mbb:
+    #             if b in bb:
+    #                 cBlue+=1
+    #         if cRed>=5 and rw[0] != row[0]:
+    #             print(rw[0], row[0], cRed, cBlue)
         
 
--- serarch_ssq.py
+++ serarch_ssq.py
@@ -20,8 +20,9 @@ if __name__ == '__main__':
     # myb = [5]
     myr = [4, 8, 9, 18, 25, 33]
     myb = [10]
-
+    all_balls = []
     for row in cursor:
+        all_balls.append(row)
         rb = []
         rb.append(row[1])
         rb.append(row[2])
@@ -43,6 +44,38 @@ if __name__ == '__main__':
             print(row[0], cRed, cBlue)
                 
     conn.close()
+
+    # print(len(all_balls))
+    # for rw in all_balls:
+    #     mrb = []
+    #     mrb.append(rw[1])
+    #     mrb.append(rw[2])
+    #     mrb.append(rw[3])
+    #     mrb.append(rw[4])
+    #     mrb.append(rw[5])
+    #     mrb.append(rw[6])
+    #     mbb = []
+    #     mbb.append(rw[7])
+    #     for row in all_balls:
+    #         rb = []
+    #         rb.append(row[1])
+    #         rb.append(row[2])
+    #         rb.append(row[3])
+    #         rb.append(row[4])
+    #         rb.append(row[5])
+    #         rb.append(row[6])
+    #         bb = [] 
+    #         bb.append(row[7])
+    #         cRed = 0
+    #         cBlue = 0
+    #         for r in mrb:
+    #             if r in rb:
+    #                 cRed+=1
+    #         for b in mbb:
+    #             if b in bb:
+    #                 cBlue+=1
+    #         if cRed>=5 and rw[0] != row[0]:
+    #             print(rw[0], row[0], cRed, cBlue)
 
         
 
