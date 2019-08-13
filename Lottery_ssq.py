bd6fe6044e722808f14d2d4c321f403221619cf7
zhuyun <zhuy@chbiomedical.com>
2019-08-13 13:37:48 +0800

    download dlt ssq

--- Lottery_ssq.py
+++ Lottery_ssq.py
@@ -6,7 +6,7 @@ import re
 import time
 
 if __name__ == '__main__':
-    url = 'http://kaijiang.500.com/shtml/dlt/{}.shtml'
+    url = 'http://kaijiang.500.com/shtml/ssq/{}.shtml'
     data = None
     header = {
         'User-Agent':
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     
     conn = sqlite3.connect('lottery.db')
     c = conn.cursor()
-    # c.execute("DELETE FROM dlt") 
-    response = requests.post(url.format('19092'), data, header)
+    # c.execute("DELETE FROM ssq") 
+    response = requests.post(url.format('19093'), data, header)
     response.encoding = 'utf-8'
     bs = BeautifulSoup(response.text, 'lxml')
     span = bs.find('span', class_='iSelectBox')
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         for ii in span.find_all('a'):
             if ii.text<='19093':
                 continue
-            
+
             print(ii.text)
             time.sleep(0.1)
             response = requests.post(url.format(ii.text), data, header)
@@ -40,11 +40,11 @@ if __name__ == '__main__':
                 b_red.append(int(th.text))
             for i, th in enumerate(table.find_all('li', class_='ball_blue')):
                 b_blue.append(int(th.text))
-            c.execute("INSERT INTO dlt(phase,r1,r2,r3,r4,r5,b1,b2) \
-            VALUES ('{}', {}, {}, {}, {}, {}, {}, {})".format(ii.text, b_red[0], b_red[1], b_red[2], b_red[3], b_red[4],b_blue[0],b_blue[1]));
+            c.execute("INSERT INTO ssq(phase,r1,r2,r3,r4,r5,r6,b1) \
+            VALUES ('{}', {}, {}, {}, {}, {}, {}, {})".format(ii.text, b_red[0], b_red[1], b_red[2], b_red[3], b_red[4], b_red[5],b_blue[0]));
     finally:
         conn.commit()
         conn.close()
-
+        
         time_end=time.time()
         print('totally cost',time_end-time_start)
--- serarch_ssq.py
+++ serarch_ssq.py
@@ -7,17 +7,19 @@ import re
 if __name__ == '__main__':
     conn = sqlite3.connect('lottery.db')
     c = conn.cursor()
-    cursor = c.execute("SELECT * FROM dlt")
+    cursor = c.execute("SELECT * FROM ssq")
     conn.commit()
-    myr = [1, 18, 19, 27, 29]
-    myb = [2, 4, 8]
+    # myr = [1, 18, 19, 27, 29, 33]
+    # myb = [13]
 
-    # myr = [2, 15, 17, 25, 31]
-    # myb = [3, 6]
-    # myr = [1, 7, 19, 29, 32]
-    # myb = [3, 8]
-    # myr = [5, 10, 14, 17, 31]
-    # myb = [4, 5]
+    # myr = [5, 7, 8, 9, 20, 22]
+    # myb = [2]
+    # myr = [1, 13, 17, 19, 27, 30]
+    # myb = [12]
+    # myr = [1, 9, 10, 12, 17, 25]
+    # myb = [5]
+    myr = [4, 8, 9, 18, 25, 33]
+    myb = [10]
 
     for row in cursor:
         rb = []
@@ -26,8 +28,8 @@ if __name__ == '__main__':
         rb.append(row[3])
         rb.append(row[4])
         rb.append(row[5])
+        rb.append(row[6])
         bb = []
-        bb.append(row[6])
         bb.append(row[7])
         cRed = 0
         cBlue = 0
@@ -37,7 +39,7 @@ if __name__ == '__main__':
         for b in myb:
             if b in bb:
                 cBlue+=1
-        if cRed>=3:
+        if cRed>=4:
             print(row[0], cRed, cBlue)
                 
     conn.close()
