447263475d00a33e01b13d7f1fe0de32baa6305f
zhuyun <zhuy@chbiomedical.com>
2019-08-14 18:11:33 +0800

    matplot

--- serarch_ssq.py
+++ serarch_ssq.py
@@ -4,13 +4,21 @@ import json
 import sqlite3
 import re
 
+import matplotlib.pyplot as plt
+
 if __name__ == '__main__':
+
+    val = [0]*33
+
+    index = list(range(1, 33+1))
+
+
     conn = sqlite3.connect('lottery.db')
     c = conn.cursor()
-    cursor = c.execute("SELECT * FROM ssq")
+    cursor = c.execute("SELECT * FROM ssq ORDER BY phase DESC")
     conn.commit()
-    # myr = [1, 18, 19, 27, 29, 33]
-    # myb = [13]
+    myr = [1, 18, 19, 27, 29, 33]
+    myb = [13]
 
     # myr = [5, 7, 8, 9, 20, 22]
     # myb = [2]
@@ -18,11 +26,8 @@ if __name__ == '__main__':
     # myb = [12]
     # myr = [1, 9, 10, 12, 17, 25]
     # myb = [5]
-    myr = [4, 8, 9, 18, 25, 33]
-    myb = [10]
-    all_balls = []
+
     for row in cursor:
-        all_balls.append(row)
         rb = []
         rb.append(row[1])
         rb.append(row[2])
@@ -30,8 +35,18 @@ if __name__ == '__main__':
         rb.append(row[4])
         rb.append(row[5])
         rb.append(row[6])
+        val[row[1]-1] += 1
+        val[row[2]-1] += 1
+        val[row[3]-1] += 1
+        val[row[4]-1] += 1
+        val[row[5]-1] += 1
+        val[row[6]-1] += 1
+
+        ###
         bb = []
         bb.append(row[7])
+        
+
         cRed = 0
         cBlue = 0
         for r in myr:
@@ -40,42 +55,14 @@ if __name__ == '__main__':
         for b in myb:
             if b in bb:
                 cBlue+=1
-        if cRed>=4:
+        if cRed>=3 or cBlue == 1:
             print(row[0], cRed, cBlue)
                 
     conn.close()
 
-    # print(len(all_balls))
-    # for rw in all_balls:
-    #     mrb = []
-    #     mrb.append(rw[1])
-    #     mrb.append(rw[2])
-    #     mrb.append(rw[3])
-    #     mrb.append(rw[4])
-    #     mrb.append(rw[5])
-    #     mrb.append(rw[6])
-    #     mbb = []
-    #     mbb.append(rw[7])
-    #     for row in all_balls:
-    #         rb = []
-    #         rb.append(row[1])
-    #         rb.append(row[2])
-    #         rb.append(row[3])
-    #         rb.append(row[4])
-    #         rb.append(row[5])
-    #         rb.append(row[6])
-    #         bb = [] 
-    #         bb.append(row[7])
-    #         cRed = 0
-    #         cBlue = 0
-    #         for r in mrb:
-    #             if r in rb:
-    #                 cRed+=1
-    #         for b in mbb:
-    #             if b in bb:
-    #                 cBlue+=1
-    #         if cRed>=5 and rw[0] != row[0]:
-    #             print(rw[0], row[0], cRed, cBlue)
-
-        
+    plt.bar(index, val)
+    plt.xticks(index, ['1','2','3','4','5'])
+    plt.xlabel('index')
+    plt.ylabel('val')
+    plt.show()
 
